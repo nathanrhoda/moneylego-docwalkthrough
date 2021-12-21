116e4f713a91850accc8b1324d8e01e597dddfa9
Nathan Rhoda <nathan.rhoda@hotmail.com>
2021-12-21 18:01:01 +0200

    quickstart

---
+++ javascript/src/money-lego.js
@@ -0,0 +1,9 @@
+import { legos } from "@studydefi/money-legos";
+
+// // protocols
+// legos.uniswap.factory.abi;
+// legos.uniswap.factory.address;
+
+// // erc20 tokens
+// legos.erc20.abi;
+// legos.erc20.dai.address;
